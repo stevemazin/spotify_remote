8f3d8fc612f4f60ad174f0ec6273811b32b85bea
stevemazin <munenekinyua.mk@gmail.com>
2021-03-02 11:43:06 +0300

    created functionality for the create room page

--- api/views.py
+++ api/views.py
@@ -16,7 +16,7 @@ class CreateRoomView(APIView):
 
     def post (self, request, format=None):
         if not self.request.session.exists(self.request.session.session_key):
-            self.request.session.creat()
+            self.request.session.create()
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             guest_can_pause = serializer.data.get('guest_can_pause')
@@ -29,8 +29,12 @@ class CreateRoomView(APIView):
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
                 room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
             else:
                 room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
                 room.save()
+                return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
-        return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
+
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
